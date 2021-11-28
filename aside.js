29ad81daa5c6b343efc2e63fc7c881ac39dfbc76
fahadshoukat <fraza6970@gmail.com>
2021-11-28 14:20:35 +0500

    addAndAccessShowSideBarClass

--- aside.js
+++ aside.js
@@ -1,3 +1,7 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector(".close-btn");
 const sideBar = document.querySelector(".sideBar");
+
+toggleBtn.addEventListener('click', function(){
+    sideBar.classList.toggle('show-sideBar');
+});
